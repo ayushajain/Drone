84b3b7946d8891b7b08a84a5a63d021639d0ae3f
Ayush Jain <ayushajain@gmail.com>
2016-06-22 20:00:58 -0700

    added android app

---
+++ Drone-Flash Android/app/src/main/java/com/example/ayush/droneflashing/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.ayush.droneflashing;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //rate of each bit in milliseconds
+    private final int TIC_RATE = 500;
+
+    //list containing all the fields
+    ArrayList<EditText> fields = new ArrayList<EditText>();
+
+
+    //flash related variables
+    private boolean isFlashOn;
+    private CameraManager manager;
+    private Camera cam;
+    private Timer timer;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button)findViewById(R.id.ButtonStart);
+        Button btnReset = (Button)findViewById(R.id.ButtonReset);
+
+        //access all the fields
+        fields.add((EditText)findViewById(R.id.Field1));
+        fields.add((EditText)findViewById(R.id.Field2));
+        fields.add((EditText)findViewById(R.id.Field3));
+        fields.add((EditText)findViewById(R.id.Field4));
+        fields.add((EditText)findViewById(R.id.Field5));
+        fields.add((EditText)findViewById(R.id.Field6));
+        fields.add((EditText)findViewById(R.id.Field7));
+        fields.add((EditText)findViewById(R.id.Field8));
+
+        //setting up Camera
+        manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        isFlashOn = false;
+
+        //reset button listener
+        if (btnReset != null) {
+            btnReset.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    //turn off timer
+                    if(timer != null) {
+                        timer.cancel();
+                        timer.purge();
+                    }
+
+                    turnFlashOff();
+
+                    //reset all field values;
+                    for(EditText field: fields)
+                        field.setText("0");
+
+                }
+            });
+        }
+
+        //start button listener
+        if (btnStart != null) {
+            btnStart.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    //reset timer
+                    if(timer != null) {
+                        timer.cancel();
+                        timer.purge();
+                    }
+
+                    timer = new Timer();
+                    timer.scheduleAtFixedRate(new TimerTask() {
+
+                        int count = 0;
+
+                        @Override
+                        public void run() {
+
+                            //check whether each field is equal to zero
+                            if(fields.get(count).getText().toString().equals("0"))
+                                turnFlashOff();
+                            else
+                                turnFlashOn();
+
+                            //iterate through each of the fields
+                            count++;
+
+                            //reset current iteration whenever necessary
+                            if(count >= fields.size())
+                                count = 0;
+
+                        }
+                    }, 0, TIC_RATE);
+                }
+            });
+        }
+    }
+
+    //TODO: Figure out what is causing Error 1001.(Probably something related to the old Camera API)
+
+    private void turnFlashOn(){
+
+        if(!isFlashOn) {
+
+            //if user is on marshmallow, use latest torch api
+            if (android.os.Build.VERSION.SDK_INT >= 23) {
+                try {
+                    manager.setTorchMode(manager.getCameraIdList()[0], true);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if(cam == null)
+                    cam = Camera.open();
+                Camera.Parameters p = cam.getParameters();
+                p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                cam.setParameters(p);
+                cam.startPreview();
+            }
+        }
+
+        isFlashOn = true;
+    }
+
+    private void turnFlashOff(){
+
+        if(isFlashOn) {
+
+            //if user is on marshmallow, use latest torch api
+            if (android.os.Build.VERSION.SDK_INT >= 23) {
+                try {
+                    manager.setTorchMode(manager.getCameraIdList()[0], false);
+                    isFlashOn = false;
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if(cam == null)
+                    cam = Camera.open();
+                Camera.Parameters p = cam.getParameters();
+                p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                cam.setParameters(p);
+                cam.stopPreview();
+            }
+        }
+        isFlashOn = false;
+    }
+}
--- Image Processing/flash-detection.py
+++ Image Processing/flash-detection.py
@@ -15,7 +15,6 @@ args = vars(ap.parse_args())
 
 body_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv3/3.1.0_1/share/OpenCV/haarcascades/haarcascade_fullbody.xml')
 
-
 def performFilters(frame):
 
     # kernel for high pass filters
@@ -29,11 +28,11 @@ def performFilters(frame):
     (T, binaryThresh) = cv2.threshold(blurred, float(args["threshold"]), 255, cv2.THRESH_BINARY)
     (T, toZeroInvThresh) = cv2.threshold(blurred, float(args["threshold"]), 255, cv2.THRESH_TOZERO_INV)
 
-    mask = cv2.dilate(binaryThresh, None, iterations=3)
+    mask = cv2.dilate(binaryThresh, None, iterations=5)
 
     body = body_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in body:
-         cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
 
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -41,11 +40,11 @@ def performFilters(frame):
         peri = cv2.arcLength(c, True);
         approx = cv2.approxPolyDP(c, 0.04 * peri, True)
         if len(c) < 50 and len(c) > 5:
-            cv2.drawContours(gray, [c], -1, (0,255,0), 2)
+            cv2.drawContours(frame, [c], -1, (0,255,0), 2)
 
     filtered = {}
     filtered['binaryThresh'] = mask
-    filtered['toZeroInvThresh'] = gray
+    filtered['toZeroInvThresh'] = frame
 
     return filtered
 
@@ -57,14 +56,22 @@ if args["image"] is "foo" or args["video"]:
         cap = cv2.VideoCapture(args["image"])
     else:
         cap = cv2.VideoCapture(0)
+    
+    w=int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH ))
+    h=int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT ))
+    # video recorder
+    fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')  # cv2.VideoWriter_fourcc() does not exist
+    out = cv2.VideoWriter("output.mp4", fourcc, 25, (w, h))
 
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
 
         result = performFilters(frame)
-        cv2.imshow('BINARY_FILTER', result['binaryThresh'])
-        cv2.imshow('TOZERO_INVERSE_FILTER', result['toZeroInvThresh'])
+        # cv2.imshow('BINARY_FILTER', result['binaryThresh'])
+        # cv2.imshow('TOZERO_INVERSE_FILTER', result['toZeroInvThresh'])
+        
+        out.write(result['toZeroInvThresh'])
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -81,4 +88,6 @@ else:
     cv2.waitKey(0)
 
 
-
+cap.release()
+out.release()
+cv2.destroyAllWindows()
